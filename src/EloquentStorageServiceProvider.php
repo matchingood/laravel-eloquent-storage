0373bfb9e330dd29ed289eee2cab8ab4db1c76f9
mg-inukai <f.inukai@mybrainlab.net>
2025-02-20 12:31:20 +0900

    refactor: macroの中身をクラスメソッド化

--- src/EloquentStorageServiceProvider.php
+++ src/EloquentStorageServiceProvider.php
@@ -11,18 +11,7 @@ class EloquentStorageServiceProvider extends ServiceProvider
     public function boot()
     {
         Response::macro('downloadEloquentStorage', function (EloquentStorage $model, $status = 200) {
-            try {
-                $content = $model->getContent();
-            } catch (FileNotFoundException $e) {
-                abort(404);
-            }
-            $fileName = $model->file_name;
-            $fileNameSjisWin = mb_convert_encoding($fileName, 'SJIS-win', 'UTF-8');
-            $headers = [
-                'Content-Type' => 'application/octet-stream',
-                'Content-disposition' => "attachment; filename={$fileNameSjisWin}",
-            ];
-            return Response::make($content, $status, $headers);
+            return EloquentStorageServiceProvider::createFileDownloadRequest($model, $status);
         });
 
         Response::macro('openEloquentStorage', function (EloquentStorage $model, $status = 200, $mimeType = null) {
@@ -32,6 +21,22 @@ class EloquentStorageServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../config/eloquentstorage.php' => config_path('eloquentstorage.php')
         ]);
+    }
+
+    public static function createFileDownloadRequest(EloquentStorage $model, $status = 200)
+    {
+        try {
+            $content = $model->getContent();
+        } catch (FileNotFoundException $e) {
+            abort(404);
+        }
+        $fileName = $model->file_name;
+        $fileNameSjisWin = mb_convert_encoding($fileName, 'SJIS-win', 'UTF-8');
+        $headers = [
+            'Content-Type' => 'application/octet-stream',
+            'Content-disposition' => "attachment; filename={$fileNameSjisWin}",
+        ];
+        return Response::make($content, $status, $headers);
     }
 
     public static function createFileOpenRequest(EloquentStorage $model, $status = 200, $mimeType = null)
